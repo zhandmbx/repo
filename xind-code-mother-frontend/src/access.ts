188c621e9efd133a82c1817c1f8175e467c69aac
zhandmbx <zhandmbx@163.com>
2025-09-16 22:48:08 +0800

    前端用户管理模块代码提交

---
+++ xind-code-mother-frontend/src/access.ts
@@ -0,0 +1,37 @@
+/*
+ * @Author: zhandmbx zhandmbx@163.com
+ * @Date: 2025-09-15 22:57:56
+ * @LastEditors: zhandmbx zhandmbx@163.com
+ * @LastEditTime: 2025-09-15 22:58:14
+ * @FilePath: \xind-code-mother-frontend\src\access.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { useLoginUserStore } from '@/stores/loginUser'
+import { message } from 'ant-design-vue'
+import router from '@/router'
+
+// 是否为首次获取登录用户
+let firstFetchLoginUser = true
+
+/**
+ * 全局权限校验
+ */
+router.beforeEach(async (to, from, next) => {
+  const loginUserStore = useLoginUserStore()
+  let loginUser = loginUserStore.loginUser
+  // 确保页面刷新，首次加载时，能够等后端返回用户信息后再校验权限
+  if (firstFetchLoginUser) {
+    await loginUserStore.fetchLoginUser()
+    loginUser = loginUserStore.loginUser
+    firstFetchLoginUser = false
+  }
+  const toUrl = to.fullPath
+  if (toUrl.startsWith('/admin')) {
+    if (!loginUser || loginUser.userRole !== 'admin') {
+      message.error('没有权限')
+      next(`/user/login?redirect=${to.fullPath}`)
+      return
+    }
+  }
+  next()
+})
---
+++ xind-code-mother-frontend/src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Author: zhandmbx zhandmbx@163.com
+ * @Date: 2025-09-13 16:02:26
+ * @LastEditors: zhandmbx zhandmbx@163.com
+ * @LastEditTime: 2025-09-14 22:30:54
+ * @FilePath: \xind-code-mother-frontend\src\pages\HomePage.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <main>
+    主页
+  </main>
+</template>
